cc52c6503f8743e2518204e2a6eca56114b00419
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-09-24 01:39:41 -0300

    Gulp watch js and css modifications

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,27 @@
 'use strict';
 
-var watchify = require('watchify');
-var browserify = require('browserify');
 var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var gutil = require('gulp-util');
-var assign = require('lodash.assign');
+var minify = require('gulp-minify-css');
 
-var customOpts = {
-  entries: ['./src/main/webapp/resources/scripts/main.js'],
-  debug: true
-};
-var opts = assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts)); 
+gulp.task('js', function(){
+   gulp.src('./src/main/webapp/resources/scripts/**/*.js')
+   .pipe(concat('scripts.js'))
+   .pipe(uglify())
+   .pipe(gulp.dest('./src/main/webapp/resources/dist'));
+});
 
-// add transformations here
-// i.e. b.transform(coffeeify);
+gulp.task('css', function(){
+   gulp.src('./src/main/webapp/resources/css/**/*.css')
+   .pipe(concat('styles.css'))
+   .pipe(minify())
+   .pipe(gulp.dest('./src/main/webapp/resources/dist'));
+});
 
-gulp.task('js', bundle); // so you can run `gulp js` to build the file
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', gutil.log); // output build logs to terminal
+gulp.task('watch', function() {
+  gulp.watch('./src/main/webapp/resources/css/**/*.css', ['css']);
+  gulp.watch('./src/main/webapp/resources/scripts/**/*.js', ['js']);
+});
 
-function bundle() {
-  return b.bundle()
-    // log errors if they happen
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('bundle.js'))
-    // optional, remove if you don't need to buffer file contents
-    .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-       // Add transformation tasks to the pipeline here.
-    .pipe(uglify())
-    .pipe(sourcemaps.write('./')) // writes .map file
-    .pipe(gulp.dest('./dist'));
-}
+gulp.task('default', ['watch', 'js', 'css']);
